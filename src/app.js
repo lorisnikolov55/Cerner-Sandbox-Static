7e3def0e533f52402bc9757307a9d7438e2402f8
lorisnikolov55 <loris.nikolov@ucdconnect.ie>
2022-09-30 17:39:18 -0500

    created app.js

---
+++ src/app.js
@@ -0,0 +1,14 @@
+async function makeRequests(){
+    var patient = await fetch(myApp.smart.state.serverURL+"/Patient/"+myAPP.smart.patient.id, {
+        headers: {
+            Accept: "application/json+fhir",
+            Authorization: "Bearer "+myApp.smart.state.tokenResponse.access_token
+        }
+    }).then(function(data){
+        return data
+    })
+
+    var response = await patient.json()
+    console.log(response)
+    $("#patient").text(JSON.stringify(response, null, '\t'))
+}
