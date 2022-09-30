0e5bb95bfaf45abdb407a77766cbca6e91ace1f7
lorisnikolov55 <loris.nikolov@ucdconnect.ie>
2022-09-30 18:01:47 -0500

    fixing request URL

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 async function makeRequests(){
-    var patient = await fetch(myApp.smart.state.serverURL+"/Patient/"+myApp.smart.patient.id, {
+    //var patient = await fetch(myApp.smart.state.serverURL+"/Patient/"+myApp.smart.patient.id, {
+    base_url = "https://fhir-myrecord.cerner.com/dstu2/ec2458f2-1e24-41c8-b71b-0e701af7583d"
+    var patient = await fetch(base_url+"/Patient/"+myApp.smart.patient.id,{
         headers: {
             Accept: "application/json+fhir",
             Authorization: "Bearer "+myApp.smart.state.tokenResponse.access_token
