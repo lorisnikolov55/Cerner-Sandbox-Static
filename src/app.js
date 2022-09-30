9454304f46cc985b17d58cef06044c22f367ada1
lorisnikolov55 <loris.nikolov@ucdconnect.ie>
2022-09-30 17:44:15 -0500

    fixing app.html typo

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 async function makeRequests(){
-    var patient = await fetch(myApp.smart.state.serverURL+"/Patient/"+myAPP.smart.patient.id, {
+    var patient = await fetch(myApp.smart.state.serverURL+"/Patient/"+myApp.smart.patient.id, {
         headers: {
             Accept: "application/json+fhir",
             Authorization: "Bearer "+myApp.smart.state.tokenResponse.access_token
