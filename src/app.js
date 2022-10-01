a67a5c3c33dda370c8bd3a3526a01d4b657dc768
lorisnikolov55 <loris.nikolov@ucdconnect.ie>
2022-09-30 23:53:30 -0500

    testing app.js

--- src/app.js
+++ src/app.js
@@ -19,12 +19,12 @@ async function requestPatientData() {
 
 async function requestImmunizationData() {
     base_url = "https://fhir-myrecord.cerner.com/dstu2/ec2458f2-1e24-41c8-b71b-0e701af7583d"
-    var patient = await fetch(base_url+"/Immunization?patient="+myApp.smart.patient.id,{
+    var immunization = await fetch(base_url+"/Immunization?patient="+myApp.smart.patient.id,{
         headers: {
             Accept: "application/json+fhir",
             Authorization: "Bearer "+myApp.smart.state.tokenResponse.access_token
         }
-    }).then(handleResponse(response))
+    }).then(handleResponse(immunization))
 }
     
 function handleResponse(response) {
