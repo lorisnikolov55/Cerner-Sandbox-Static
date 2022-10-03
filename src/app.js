093af30b713aa3066bc4ce959344b92bd1b8feac
lorisnikolov55 <loris.nikolov@ucdconnect.ie>
2022-10-03 16:40:49 -0500

    testing patient

--- src/app.js
+++ src/app.js
@@ -97,54 +97,43 @@
 (function (window) {
     /***** Data fetching function *****/
     window.extractData = function () {
-      var ret = $.Deferred();
-  
-      function onError() {
-        console.log("Loading error", arguments);
-        ret.reject();
-      }
-  
-      function onReady() {
-        if (smart.hasOwnProperty("patient")) {
-            base_url = "https://fhir-myrecord.cerner.com/dstu2/ec2458f2-1e24-41c8-b71b-0e701af7583d"
-            var patient = fetch(base_url+"/Patient/"+myApp.smart.patient.id,{
-            headers: {
-                Accept: "application/json+fhir",
-                Authorization: "Bearer "+myApp.smart.state.tokenResponse.access_token
-            }
-            }).then((response) => {
-                if (response.ok) {
-                    return response.json()
-                } else {
-                    throw new Error("Bad HTTP stuff!")
-                }
-            }).then((jsonData) => {
-                console.log(jsonData)
-                var fname = jsonData.name[0].given[0]
-                var lname = jsonData.name[0].family[0]
-                var gender = jsonData.gender
-                var dob = new Date(jsonData.birthDate)
-                var day = dob.getDate()
-                var monthIndex = dob.getMonth() + 1
-                var year = dob.getFullYear()
-                var dobStr = monthIndex + "/" + day + "/" + year
-
-                var p = defaultPatient()
-                p.birthdate = dobStr
-                p.gender = gender
-                p.fname = fname
-                p.lname = lname
-
-                console.log(p)
-                ret.resolve(p);
-            });
-        } else {
-          onError();
+        var ret = $.Deferred();
+        
+        base_url = "https://fhir-myrecord.cerner.com/dstu2/ec2458f2-1e24-41c8-b71b-0e701af7583d"
+        fetch(base_url+"/Patient/"+myApp.smart.patient.id,{
+        headers: {
+            Accept: "application/json+fhir",
+            Authorization: "Bearer "+myApp.smart.state.tokenResponse.access_token
         }
-      }
-  
-      FHIR.oauth2.ready(onReady, onError);
-      return ret.promise();
+        }).then((response) => {
+            if (response.ok) {
+                return response.json()
+            } else {
+                throw new Error("Bad HTTP stuff!")
+            }
+        }).then((jsonData) => {
+            console.log(jsonData)
+            var fname = jsonData.name[0].given[0]
+            var lname = jsonData.name[0].family[0]
+            var gender = jsonData.gender
+            var dob = new Date(jsonData.birthDate)
+            var day = dob.getDate()
+            var monthIndex = dob.getMonth() + 1
+            var year = dob.getFullYear()
+            var dobStr = monthIndex + "/" + day + "/" + year
+
+            var p = defaultPatient()
+            p.birthdate = dobStr
+            p.gender = gender
+            p.fname = fname
+            p.lname = lname
+
+            console.log(p)
+            ret.resolve(p);
+        });
+        
+        FHIR.oauth2.ready();
+        return ret.promise();
     };
   
     /***** Patient object definition *****/
