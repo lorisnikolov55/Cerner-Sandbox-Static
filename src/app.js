de395d3f69930ac7d50bfd37c55daa480ba02465
lorisnikolov55 <loris.nikolov@ucdconnect.ie>
2022-10-03 16:06:59 -0500

    testing patient

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-async function extractData() {
+/*async function extractData() {
     base_url = "https://fhir-myrecord.cerner.com/dstu2/ec2458f2-1e24-41c8-b71b-0e701af7583d"
     var patient = await fetch(base_url+"/Patient/"+myApp.smart.patient.id,{
         headers: {
@@ -69,7 +69,7 @@ function defaultPatient() {
 }
 
 /***** HTML indexing *****/
-drawVisualization = function (p) {
+/*drawVisualization = function (p) {
     console.log(p.fname)
     console.log(typeof p.fname)
     // Patient data
@@ -82,7 +82,7 @@ drawVisualization = function (p) {
   };
 
 /***** HTML indexing *****/
-function drawVisualization(p) {
+/*function drawVisualization(p) {
     console.log(p.fname)
     console.log(typeof p.fname)
     // Patient data
@@ -92,13 +92,11 @@ function drawVisualization(p) {
     $("#lname").html(p.lname);
     $("#gender").html(p.gender);
     $("#birthdate").html(p.birthdate);
-  };
+  };*/
 
-
-
-/*(function (window) {
+(function (window) {
     /***** Data fetching function *****/
-    /*window.extractData = function () {
+    window.extractData = function () {
       var ret = $.Deferred();
   
       function onError() {
@@ -106,136 +104,48 @@ function drawVisualization(p) {
         ret.reject();
       }
   
-      function onReady(smart) {
+      function onReady() {
         if (smart.hasOwnProperty("patient")) {
-          var patient = smart.patient;
-          var pt = patient.read();
+            var patient = smart.patient;
+            var pt = patient.read();
   
-          const uri =
-            "https://fhir-open.cerner.com/dstu2/ec2458f2-1e24-41c8-b71b-0e701af7583d/Immunization?patient=" +
-            String(patient.id);
-          console.log(uri);
-          let h = new Headers();
-          h.append("Accept", "application/json+fhir");
+            base_url = "https://fhir-myrecord.cerner.com/dstu2/ec2458f2-1e24-41c8-b71b-0e701af7583d"
+            var patient = await fetch(base_url+"/Patient/"+myApp.smart.patient.id,{
+            headers: {
+                Accept: "application/json+fhir",
+                Authorization: "Bearer "+myApp.smart.state.tokenResponse.access_token
+            }
+        }).then((response) => {
+            if (response.ok) {
+                return response.json()
+            } else {
+                throw new Error("Bad HTTP stuff!")
+            }
+        }).then((jsonData) => {
+            console.log(jsonData)
+        });
   
-          let req = new Request(uri, {
-            method: "GET",
-            headers: h,
-            mode: "cors",
-          });
+        $.when(pt).fail(onError);
   
-          fetch(req)
-            .then((response) => {
-              if (response.ok) {
-                return response.json();
-              } else {
-                throw new Error("Bad HTTP stuff!");
-              }
-            })
-            .then((jsonData) => {
-              console.log(jsonData.entry[0]);
-              var vaccineCode = undefined;
-              var vaccineManufacturer = undefined;
-              var vaccineStatus = undefined;
-              var doseQuantity = undefined;
-              var dateGiven = undefined;
-              var expiryDate = undefined;
-  
-              if (jsonData.entry[0].resource.hasOwnProperty("vaccineCode")) {
-                vaccineCode = jsonData.entry[0].resource.vaccineCode.text;
-                console.log(vaccineCode);
-              } else {
-                vaccineCode = "NA";
-                console.log(vaccineCode);
-              }
-  
-              if (jsonData.entry[0].resource.hasOwnProperty("manufacturer")) {
-                vaccineManufacturer =
-                  jsonData.entry[0].resource.manufacturer.display;
-                console.log(vaccineManufacturer);
-              } else {
-                vaccineManufacturer = "NA";
-                console.log(vaccineManufacturer);
-              }
-  
-              if (jsonData.entry[0].resource.hasOwnProperty("status")) {
-                vaccineStatus = jsonData.entry[0].resource.status;
-                console.log(vaccineStatus);
-              } else {
-                vaccineStatus = "NA";
-                console.log(vaccineStatus);
-              }
-  
-              if (jsonData.entry[0].resource.hasOwnProperty("doseQuantity")) {
-                if (
-                  typeof String(jsonData.entry[0].resource.doseQuantity.value) ||
-                  jsonData.entry[0].resource.doseQuantity.unit !== "unknown unit"
-                ) {
-                  doseQuantity =
-                    String(jsonData.entry[0].resource.doseQuantity.value) +
-                    " " +
-                    jsonData.entry[0].resource.doseQuantity.unit;
-                  console.log(doseQuantity);
-                } else {
-                  doseQuantity = "NA";
-                  console.log(doseQuantity);
-                }
-              } else {
-                doseQuantity = "NA";
-                console.log(doseQuantity);
-              }
-  
-              if (jsonData.entry[0].resource.hasOwnProperty("date")) {
-                dateGiven = jsonData.entry[0].resource.date;
-                console.log(dateGiven);
-              } else {
-                dateGiven = "NA";
-                console.log(dateGiven);
-              }
-  
-              if (jsonData.entry[0].resource.hasOwnProperty("expirationDate")) {
-                expiryDate = jsonData.entry[0].resource.expirationDate;
-                console.log(expiryDate);
-              } else {
-                expiryDate = "NA";
-                console.log(expiryDate);
-              }
-  
-              $.when(pt).fail(onError);
-  
-              $.when(pt).done(function (patient) {
-                var gender = patient.gender;
-                var dob = new Date(patient.birthDate);
-                var day = dob.getDate();
-                var monthIndex = dob.getMonth() + 1;
-                var year = dob.getFullYear();
-  
-                var dobStr = monthIndex + "/" + day + "/" + year;
-                var fname = "";
-                var lname = "";
-  
-                if (typeof patient.name[0] !== "undefined") {
-                  fname = patient.name[0].given.join(" ");
-                  lname = patient.name[0].family.join(" ");
-                }
-  
-                var p = defaultPatient();
-                p.birthdate = dobStr;
-                p.gender = gender;
-                p.fname = fname;
-                p.lname = lname;
-  
-                // Immunizations
-                p.vCode = vaccineCode;
-                p.vManufacturer = vaccineManufacturer;
-                p.vStatus = vaccineStatus;
-                p.vDoseQuantity = doseQuantity;
-                p.vDateGiven = dateGiven;
-                p.vExpiryDate = expiryDate;
-  
-                ret.resolve(p);
-              });
-            });
+        $.when(pt).done(function (patient) {
+            var fname = jsonData.name[0].given[0]
+            var lname = jsonData.name[0].family[0]
+            var gender = jsonData.gender
+            var dob = new Date(jsonData.birthDate)
+            var day = dob.getDate()
+            var monthIndex = dob.getMonth() + 1
+            var year = dob.getFullYear()
+            var dobStr = monthIndex + "/" + day + "/" + year
+
+            var p = defaultPatient()
+            p.birthdate = dobStr
+            p.gender = gender
+            p.fname = fname
+            p.lname = lname
+
+            console.log(p)
+            ret.resolve(p);
+        });
         } else {
           onError();
         }
@@ -244,5 +154,26 @@ function drawVisualization(p) {
       FHIR.oauth2.ready(onReady, onError);
       return ret.promise();
     };
-    
-  })(window);*/
+  
+    /***** Patient object definition *****/
+    function defaultPatient() {
+      return {
+        // Patient data
+        fname: { value: "" },
+        lname: { value: "" },
+        gender: { value: "" },
+        birthdate: { value: "" },
+      };
+    }
+  
+    /***** HTML indexing *****/
+    window.drawVisualization = function (p) {
+      // Patient data
+      $("#holder").show();
+      $("#loading").hide();
+      $("#fname").html(p.fname);
+      $("#lname").html(p.lname);
+      $("#gender").html(p.gender);
+      $("#birthdate").html(p.birthdate);
+    };
+})(window);
