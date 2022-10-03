f1b95f6f522b33a8f511ebfdb65c4ae7e88477e9
lorisnikolov55 <loris.nikolov@ucdconnect.ie>
2022-10-03 18:16:32 -0500

    trying to return patient data

--- src/app.js
+++ src/app.js
@@ -6,6 +6,37 @@ async function requestPatientData() {
             Authorization: "Bearer "+myApp.smart.state.tokenResponse.access_token
         }
     })
+
     var response = await patient.json() 
     console.log(response)
-}          
+
+    var fname = response.name[0].given[0]
+    var lname = response.name[0].family[0]
+    var gender = response.gender
+    
+    var dob = new Date(patient.birthDate);
+    var day = dob.getDate();
+    var monthIndex = dob.getMonth() + 1;
+    var year = dob.getFullYear();
+
+    var dobStr = monthIndex + "/" + day + "/" + year;
+
+    var p = defaultPatient()
+    p.fname = fname
+    p.lname = lname
+    p.gender = gender
+    p.birthdate = dobStr
+    
+    return p
+}        
+
+/***** Patient object definition *****/
+function defaultPatient() {
+    return {
+      // Patient data
+      fname: { value: "" },
+      lname: { value: "" },
+      gender: { value: "" },
+      birthdate: { value: "" },
+    }
+}
