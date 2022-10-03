f030f8f9cdf79abb16e4eb0ae9bd28dba541559e
lorisnikolov55 <loris.nikolov@ucdconnect.ie>
2022-10-03 16:47:41 -0500

    testing patient

--- src/app.js
+++ src/app.js
@@ -96,11 +96,11 @@
 
 (function (window) {
     /***** Data fetching function *****/
-    window.extractData = function () {
+    window.extractData = async function () {
         var ret = $.Deferred();
         
         base_url = "https://fhir-myrecord.cerner.com/dstu2/ec2458f2-1e24-41c8-b71b-0e701af7583d"
-        fetch(base_url+"/Patient/"+myApp.smart.patient.id,{
+        var patient = await fetch(base_url+"/Patient/"+myApp.smart.patient.id,{
         headers: {
             Accept: "application/json+fhir",
             Authorization: "Bearer "+myApp.smart.state.tokenResponse.access_token
