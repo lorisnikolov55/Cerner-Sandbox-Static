9f2de402d634edccbe2a2e8fa60318a32228ded0
lorisnikolov55 <loris.nikolov@ucdconnect.ie>
2022-10-03 16:11:11 -0500

    testing patient

--- src/app.js
+++ src/app.js
@@ -110,7 +110,7 @@ function defaultPatient() {
             var pt = patient.read();
   
             base_url = "https://fhir-myrecord.cerner.com/dstu2/ec2458f2-1e24-41c8-b71b-0e701af7583d"
-            var patient = await fetch(base_url+"/Patient/"+myApp.smart.patient.id,{
+            var patient = fetch(base_url+"/Patient/"+myApp.smart.patient.id,{
             headers: {
                 Accept: "application/json+fhir",
                 Authorization: "Bearer "+myApp.smart.state.tokenResponse.access_token
