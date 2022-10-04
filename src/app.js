6e76b2ff9e83ec9e1bcd885187829df9b2918aa2
lorisnikolov55 <loris.nikolov@ucdconnect.ie>
2022-10-04 16:02:02 -0500

    testing

--- src/app.js
+++ src/app.js
@@ -1,91 +1,60 @@
 (function (window) {
   /***** Data fetching function *****/
   window.extractData = function () {
-    if (myApp.smart.hasOwnProperty("patient")) {
-      console.log("TRUE")
-    }
-
     var ret = $.Deferred();
-    console.log("Top of extractData")
 
     function onError() {
-      console.log("In onError")
       console.log("Loading error", arguments);
       ret.reject();
     }
 
-    function onReady() {
-      if (myApp.smart.hasOwnProperty("patient")) {
+    function onReady(smart) {
+      if (smart.hasOwnProperty("patient")) {
         console.log("In onReady");
-        var patient = myApp.smart.patient;
-        //var pt = patient.read();
+        var patient = smart.patient;
 
-        /*****Making Request*****/
-        const base_url =
-          "https://fhir-myrecord.cerner.com/dstu2/ec2458f2-1e24-41c8-b71b-0e701af7583d";
-        const uri = base_url + "/Patient/" + patient.id;
-        console.log(uri);
+        const base_url = "https://fhir-myrecord.cerner.com/dstu2/ec2458f2-1e24-41c8-b71b-0e701af7583d"
+        var pt = fetch(base_url+"/Immunization?patient="+smart.patient.id,{
+        headers: {
+            Accept: "application/json+fhir",
+            Authorization: "Bearer "+smart.state.tokenResponse.access_token
+        }})
+  
+        $.when(pt).fail(onError)
 
-        let h = new Headers();
-        h.append("Accept", "application/json+fhir");
-        h.append(
-          "Authorization",
-          "Bearer " + myApp.smart.state.tokenResponse.access_token
-        );
+        $.when(pt).done(function (patient) {
+          var gender = patient.gender;
+          var dob = new Date(patient.birthDate);
+          var day = dob.getDate();
+          var monthIndex = dob.getMonth() + 1;
+          var year = dob.getFullYear();
 
-        let req = new Request(uri, {
-          method: "GET",
-          headers: h,
-          mode: "cors",
-        });
+          var dobStr = monthIndex + "/" + day + "/" + year;
+          var fname = "";
+          var lname = "";
 
-        fetch(req)
-          .then((response) => {
-            if (response.ok) {
-              console.log(response);
-              return response.json();
-            } else {
-              throw new Error("Bad HTTP stuff!");
-            }
-          })
-          .then((patient) => {
-            $.when(patient).fail(onError);
+          if (typeof patient.name[0] !== "undefined") {
+            fname = patient.name[0].given.join(" ");
+            lname = patient.name[0].family.join(" ");
+          }
 
-            $.when(patient).done(function (patient) {
-              var gender = patient.gender;
-              var dob = new Date(patient.birthDate);
-              var day = dob.getDate();
-              var monthIndex = dob.getMonth() + 1;
-              var year = dob.getFullYear();
+          var p = defaultPatient();
+          p.birthdate = dobStr;
+          p.gender = gender;
+          p.fname = fname;
+          p.lname = lname;
 
-              var dobStr = monthIndex + "/" + day + "/" + year;
-              var fname = "";
-              var lname = "";
-
-              if (typeof patient.name[0] !== "undefined") {
-                fname = patient.name[0].given.join(" ");
-                lname = patient.name[0].family.join(" ");
-              }
-
-              var p = defaultPatient();
-              p.birthdate = dobStr;
-              p.gender = gender;
-              p.fname = fname;
-              p.lname = lname;
-
-              ret.resolve(p);
-            });
-          });
+          ret.resolve(p);
+        })
       } else {
-        console.log("In the else statement")
-        onError();
-      }
-    }
+          onError();
+        }
+      } 
 
     console.log("Bottom of extractData")
     FHIR.oauth2.ready(onReady, onError)
     return ret.promise();
-  };
+  }
 
   /***** Patient object definition *****/
   window.defaultPatient = function () {
