03f3d61286fc0b1c3620a9f5142ede9ed2f10075
lorisnikolov55 <loris.nikolov@ucdconnect.ie>
2022-10-04 11:06:39 -0500

    testing new approach

--- src/app.js
+++ src/app.js
@@ -12,45 +12,60 @@
         function onReady(smart) {
             if(smart.hasOwnProperty("patient")) {
                 /*****Making Request*****/
-                base_url = "https://fhir-myrecord.cerner.com/dstu2/ec2458f2-1e24-41c8-b71b-0e701af7583d"
-                var pt = fetch(base_url+"/Patient/"+myApp.smart.patient.id,{
-                    headers: {
-                        Accept: "application/json+fhir",
-                        Authorization: "Bearer "+myApp.smart.state.tokenResponse.access_token
+                const base_url = "https://fhir-myrecord.cerner.com/dstu2/ec2458f2-1e24-41c8-b71b-0e701af7583d"
+                const uri = base_url+"/Patient/"+myApp.smart.patient.id
+                console.log(uri)
+
+                let h = new Headers()
+                h.append("Accept", "application/json+fhir")
+                h.append("Authorization", "Bearer "+myApp.smart.state.tokenResponse.access_token)
+                console.log(h)
+
+                let req = new Request(uri, {
+                    method: "GET",
+                    headers: h,
+                    mode: "cors",
+                  });
+
+                fetch(req)
+                  .then((response) => {
+                    if (response.ok) {
+                      return response.json();
+                    } else {
+                      throw new Error("Bad HTTP stuff!");
                     }
                 })
-                
-                console.log("Getting to here")
-                
-                $.when(pt).fail(onError)
-
-                $.when(pt).done(function(patient) {
-                    var gender = patient.gender
-                    var dob = new Date(patient.birthDate);
-                    var day = dob.getDate();
-                    var monthIndex = dob.getMonth() + 1;
-                    var year = dob.getFullYear();
-
-                    var dobStr = monthIndex + '/' + day + '/' + year;
-                    var fname = '';
-                    var lname = '';
-
-                    if(typeof patient.name[0] !== 'undefined') {
-                        fname = patient.name[0].given.join(' ');
-                        lname = patient.name[0].family.join(' ');
-                    }
-
-                    var p = defaultPatient()
-                    p.birthdate = dobStr
-                    p.gender = gender
-                    p.fname = fname
-                    p.lname = lname
-
-                    ret.resolve(p)
-                })
+                .then((jsonData) => {        
+                    $.when(jsonData).fail(onError);
+        
+                    $.when(jsonData).done(function (patient) {
+                      var gender = patient.gender;
+                      var dob = new Date(patient.birthDate);
+                      var day = dob.getDate();
+                      var monthIndex = dob.getMonth() + 1;
+                      var year = dob.getFullYear();
+        
+                      var dobStr = monthIndex + "/" + day + "/" + year;
+                      var fname = "";
+                      var lname = "";
+        
+                      if (typeof patient.name[0] !== "undefined") {
+                        fname = patient.name[0].given.join(" ");
+                        lname = patient.name[0].family.join(" ");
+                      }
+        
+                      var p = defaultPatient();
+                      p.birthdate = dobStr;
+                      p.gender = gender;
+                      p.fname = fname;
+                      p.lname = lname;
+        
+                      ret.resolve(p);               
+                    })
+                }) 
             } else {
                 onError()
-            }   
+            }      
         }
 
         FHIR.oauth2.ready(onReady, onError)
