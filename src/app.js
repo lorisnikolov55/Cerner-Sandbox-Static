4c467b191d717dc9d4977ee8b8c42842955701d7
lorisnikolov55 <loris.nikolov@ucdconnect.ie>
2022-10-04 16:21:34 -0500

    testing

--- src/app.js
+++ src/app.js
@@ -1,62 +1,60 @@
 /***** Data fetching function *****/
 function extractData() {
-  var ret = $.Deferred()
+  var ret = $.Deferred();
 
   //FHIR.oauth2.ready(onReady, onError)
-  FHIR.oauth2.ready()
-  .then(function(client) {
-    myApp.smart = client
-  })
+  FHIR.oauth2.ready().then(function (client) {
+    myApp.smart = client;
+  });
 
   //function onError() {
-    console.log("Loading error", arguments);
-    ret.reject();
+  console.log("Loading error", arguments);
+  ret.reject();
   //}
 
   //function onReady(myApp) {
-    if (smart.hasOwnProperty("patient")) {
-      console.log("In onReady");
-      var patient = myApp.smart.patient;
+  if (smart.hasOwnProperty("patient")) {
+    console.log("In onReady");
+    var patient = myApp.smart.patient;
 
-      const base_url =
-        "https://fhir-myrecord.cerner.com/dstu2/ec2458f2-1e24-41c8-b71b-0e701af7583d";
-      var pt = fetch(base_url + "/Immunization?patient=" + patient.id, {
-        headers: {
-          Accept: "application/json+fhir",
-          Authorization:
-            "Bearer " + myApp.smart.state.tokenResponse.access_token,
-        },
-      });
+    const base_url =
+      "https://fhir-myrecord.cerner.com/dstu2/ec2458f2-1e24-41c8-b71b-0e701af7583d";
+    var pt = fetch(base_url + "/Immunization?patient=" + patient.id, {
+      headers: {
+        Accept: "application/json+fhir",
+        Authorization: "Bearer " + myApp.smart.state.tokenResponse.access_token,
+      },
+    });
 
-      $.when(pt).fail(onError);
+    $.when(pt).fail(onError);
 
-      $.when(pt).done(function (patient) {
-        var gender = patient.gender;
-        var dob = new Date(patient.birthDate);
-        var day = dob.getDate();
-        var monthIndex = dob.getMonth() + 1;
-        var year = dob.getFullYear();
+    $.when(pt).done(function (patient) {
+      var gender = patient.gender;
+      var dob = new Date(patient.birthDate);
+      var day = dob.getDate();
+      var monthIndex = dob.getMonth() + 1;
+      var year = dob.getFullYear();
 
-        var dobStr = monthIndex + "/" + day + "/" + year;
-        var fname = "";
-        var lname = "";
+      var dobStr = monthIndex + "/" + day + "/" + year;
+      var fname = "";
+      var lname = "";
 
-        if (typeof patient.name[0] !== "undefined") {
-          fname = patient.name[0].given.join(" ");
-          lname = patient.name[0].family.join(" ");
-        }
+      if (typeof patient.name[0] !== "undefined") {
+        fname = patient.name[0].given.join(" ");
+        lname = patient.name[0].family.join(" ");
+      }
 
-        var p = defaultPatient();
-        p.birthdate = dobStr;
-        p.gender = gender;
-        p.fname = fname;
-        p.lname = lname;
+      var p = defaultPatient();
+      p.birthdate = dobStr;
+      p.gender = gender;
+      p.fname = fname;
+      p.lname = lname;
 
-        ret.resolve(p);
-      });
-    } else {
-      onError();
-    }
+      ret.resolve(p);
+    });
+  } else {
+    onError();
+  }
   //}
 
   return ret.promise();
